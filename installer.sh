2a6293e4c5bf096d8cef958d7b8f09e75c6ac1ea
Connor Boyle <connorboyle@sasktel.net>
2021-04-01 07:13:54 -0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,9 +4,8 @@ SRCDIR="$( cd -- "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd -P )"
 PCODIR="/opt/PCO"
 
 if [[ $SRCDIR = $PCODIR ]]; then
-    exit 0
+    make -C $PCODIR clean all install
+else
+    rsync -a $SRCDIR/* $PCODIR
+    make -C $PCODIR clean all install
 fi
-
-rsync -a $SRCDIR/* $PCODIR
-
-make -C $PCODIR clean all install
