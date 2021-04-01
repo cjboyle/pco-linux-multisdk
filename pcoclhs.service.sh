e4e3ef61c6739517fda01da59e18df3769d59ab8
Connor Boyle <connorboyle@sasktel.net>
2021-04-01 13:24:33 -0600

    Make pco_clhs_mgr the default
    Using with pco_clhs_svc seems to stop immediately.

--- pcoclhs.service.sh
+++ pcoclhs.service.sh
@@ -5,7 +5,7 @@
 SISO_DIR="/opt/SiliconSoftware/Runtime5.7.0"
 PCO_DIR="/opt/PCO"
 
-CMD="pco_clhs_${1:-svc}"
+CMD="pco_clhs_${1:-mgr}"
 
 if [ -z "${SISODIR5}" ]; then
     source /opt/SiliconSoftware/Runtime5.7.0/setup-siso-env.sh
